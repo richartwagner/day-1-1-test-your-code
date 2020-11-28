0b4aa2cefc1657c7e0b69797783a6e1f3f55241c
richartwagner <blackym@freenet.de>
2020-11-28 12:07:15 +0000

    Initial commit

---
+++ testing_copy.py
@@ -0,0 +1,31 @@
+def test_func():
+    #First *fork* your copy. Then copy-paste your code below this line 👇
+    #Finally click "Run" to execute the tests
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
